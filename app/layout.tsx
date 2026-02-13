a7242918b72a5f4e16430203c47899b50bad39d9
Alecktos <alexander.berlind@protonmail.com>
2026-02-13 08:42:11 +0100

    No need for two images

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,16 +32,9 @@ export default function RootLayout({
 		  <div className="max-w-screen-2xl mx-auto text-center bg-blush-lighter">
 			<Nav />
 			  <div className="relative w-full text-center">
-				  <div className="relative w-full h-120">
+				  <div className="relative w-full h-94 md:h-120">
 					  <Image
-						  className="object-cover object-top-right md:hidden"
-						  src={"/beach_11_desktop.png"}
-						  alt="Header image"
-						  fill
-						  priority
-					  />
-					  <Image
-						  className="object-cover grayscale object-top-right hidden md:block"
+						  className="object-cover object-top-right md:grayscale"
 						  src="/beach_11_desktop.png"
 						  alt="Header image"
 						  fill
